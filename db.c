e4e99853f94d93a31140cbbd04168709ecdfc3de
Cyrius <desagazancyrius@gmail.com>
2026-03-06 16:05:29 +0100

    feat: add exit

--- db.c
+++ db.c
@@ -1,3 +1,5 @@
+# include <stdlib.h>
+# include <string.h>
 # include <stdio.h>
 
 int main(void){
@@ -7,6 +9,13 @@ int main(void){
         fflush(stdout);
 
         fgets(input, 100, stdin);
+        int input_length = strlen(input);
+        if (input[input_length -1] == '\n'){
+            input[input_length - 1] = '\0';
+        }
+        if (strcmp(input, "exit") == 0){
+            exit(0);
+        }
     }
 
     return 0;
