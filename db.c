b2fb1f1a4beba886cc12afda7afc9d57d3ef3f45
Cyrius <desagazancyrius@gmail.com>
2026-03-06 15:41:54 +0100

    feat: add user input

--- db.c
+++ db.c
@@ -1,11 +1,12 @@
 # include <stdio.h>
 
-int main(void)
-{
-    while(1)
-    {
+int main(void){
+    char input[100];
+    while(1){
         printf("db >");
         fflush(stdout);
+
+        fgets(input, 100, stdin);
     }
 
     return 0;
