02cabac49719b02c0ac4c6ae8ac2d99cc3e7628b
Cyrius <desagazancyrius@gmail.com>
2026-03-07 16:40:23 +0100

    feat: handle error

--- db.c
+++ db.c
@@ -2,6 +2,10 @@
 # include <string.h>
 # include <stdio.h>
 
+
+void do_meta_command(char *input);
+
+
 int main(void){
     char input[100];
     while(1){
@@ -9,14 +13,27 @@ int main(void){
         fflush(stdout);
 
         fgets(input, 100, stdin);
+        
         int input_length = strlen(input);
+        
         if (input[input_length -1] == '\n'){
             input[input_length - 1] = '\0';
         }
-        if (strcmp(input, "exit") == 0){
-            exit(0);
+
+        if (input[0] == '.'){
+            do_meta_command(input);
+        } else {
+
         }
     }
 
     return 0;
+}
+
+void do_meta_command(char *input){
+    if (strcmp(input, ".exit") == 0){
+        exit(0);
+    } else {
+        printf("Unrecognized metacommand: %s", input);
+    }
 }
